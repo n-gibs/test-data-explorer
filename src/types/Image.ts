779720615c853bd9a9b9b3aed06f80a2ee456283
nikkig124 <nikkig124@gmail.com>
2022-09-24 15:10:36 -0700

    project set up

---
+++ src/types/Image.ts
@@ -0,0 +1,14 @@
+export type Image = {
+    id: string;
+    filename: string;
+    label: string;
+    height: number;
+    width: number;
+    dimension: number;
+    size: number
+    maxRGB: number;
+    minRGB: number;
+    red: number;
+    green: number;
+    blue: number;
+};
