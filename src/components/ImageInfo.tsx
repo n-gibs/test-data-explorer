bc5bc356a4713d7070182dc63717c7be2b44fcbb
nikkig124 <nikkig124@gmail.com>
2022-09-27 07:17:36 -0700

    flex-start re align text

--- src/components/ImageInfo.tsx
+++ src/components/ImageInfo.tsx
@@ -28,28 +28,34 @@ const ImageInfo: FC<any> = ({ image }: ImageProps): ReactElement => {
                 backgroundColor: 'whitesmoke',
                 display: 'flex',
                 justifyContent: 'center',
-                alignItems: 'center',
+                alignItems: 'flex-start',
             }}
         >
             <Grid item xs={6}>
                 {headers.map((head, idx) => {
                     return (
                         <Grid item key={idx}>
-                            <Typography variant='h6' gutterBottom
-                            sx={{fontWeight: 'bold'}}>{head}</Typography>
+                            <Typography
+                                variant='h6'
+                                gutterBottom
+                                sx={{ fontWeight: 'bold' }}
+                            >
+                                {head}
+                            </Typography>
                         </Grid>
                     );
                 })}
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={6} alignItems='flex-start'>
                 {headers.map((head, idx) => {
                     type ObjectKey = keyof typeof image;
                     const h = headers[idx] as ObjectKey;
 
                     return (
                         <Grid item key={idx}>
-                          <Typography variant='h6' gutterBottom>{image[h]}</Typography>
-
+                            <Typography variant='h6' gutterBottom>
+                                {image[h]}
+                            </Typography>
                         </Grid>
                     );
                 })}
